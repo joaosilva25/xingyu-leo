e035dd1e2e39a561e148c9b520ad1642547b6997
joaosilva25 <joaosilva2505@outlook.com>
2026-05-05 14:34:34 -0300

    fix: adjust footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,17 +1,13 @@
-
 const Footer = () => {
   return (
     <footer className="bg-black">
       <div className="max-w-7xl mx-auto px-6 py-8 flex flex-col items-center gap-4">
-        <div className="flex flex-col items-center gap-2">
-          <a href="https://www.xingyu.com.br" className="text-white text-xm hover:underline">www.xingyu.com.br</a>
-        </div>
         <p className="text-white text-xs">
           © 2026 Xingyu - Todos os direitos reservados
         </p>
       </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
