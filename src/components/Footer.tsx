d525634d501d64eb5408cbda5d054da74b740df1
João Vitor <joaosilva2505@outlook.com>
2026-05-05 17:57:51 -0300

    fix: remove text footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
     <footer className="bg-black">
       <div className="max-w-7xl mx-auto px-6 py-8 flex flex-col items-center gap-4">
         <p className="text-white text-xs">
-          © 2026 Xingyu - Todos os direitos reservados
+          © 2026 Todos os direitos reservados
         </p>
       </div>
     </footer>
