22d222615363d2aedc73087e6c46275df5bf02a6
joaosilva25 <joaosilva2505@outlook.com>
2026-05-05 14:22:17 -0300

    fix: adjust link

--- src/components/Links.tsx
+++ src/components/Links.tsx
@@ -5,10 +5,7 @@ export default function Links() {
   return (
     <section id="links" className="bg-black pb-20 pt-15">
       <div className="max-w-6xl mx-auto gap-12 flex flex-col pb-4 md:pb-24 pt-0 md:pt-14 px-2">
-        <Card
-          imageSrc={banner1}
-          link="https://www.xingyu.com.br/collections/colecao-nova-era"
-        />
+        <Card imageSrc={banner1} link=" https://www.youtube.com/@leochinabr" />
       </div>
     </section>
   );
